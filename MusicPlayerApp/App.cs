4e4e30423635f8712d61c97a65ec749d990d037e
Chinenye <c.okeke@genesystechhub.com>
2022-11-30 15:30:25 +0100

    Add project files.

---
+++ MusicPlayerApp/App.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayerApp
+{
+    internal class App
+    {
+        List<Person> morePeople = new List<Person>();
+    }
+
+   
+}
---
+++ MusicPlayerApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MusicPlayerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
